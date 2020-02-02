2f5a6364ce885b23f824fef48eef395a855832d5
Hue <tohuem@gmail.com>
2020-02-01 23:29:30 -0600

    Added github link

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -280,6 +280,9 @@ export default class App extends React.Component<AppProps, AppState> {
         <div id="footer">
           Image by <a href="https://pixabay.com/users/moinzon-2433302/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1412683" className="footer-link">Michi S</a> from 
              &nbsp;<a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1412683" className="footer-link">Pixabay</a>
+          <br />
+          <br />
+          See the project on <a href="https://github.com/hue2/Routines-Timer" className="footer-link">Github</a>
         </div>
       </div>
     );
