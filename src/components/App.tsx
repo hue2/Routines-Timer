defeee58e92e9824a29aa6ebe28406357e7ae156
hue2 <tohuem@gmail.com>
2020-08-27 10:46:44 -0500

    Moved footer section into its own component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { handleInput } from '../helpers/Helper';
 import Clock from './Clock';
 import ClockOptions from './ClockOptions';
-import { defaultBreak, defaultRepeat, defaultTime, defaultUIOptions } from './DefaultStates';
 import Spinner from './display_components/Spinner';
 import StartButton from './StartButton';
 import TimerOption from './TimerOptions';
 import { AppState } from './TimeType';
 import { getDisplayBreakTime, getDisplayTime, getTotalBreakSeconds, getTotalSeconds } from '../helpers/TimeHelper';
+import { defaultBreak, defaultRepeat, defaultTime, defaultUIOptions } from './DefaultStates';
 
 import '../styles/Site.css';
+import Footer from './display_components/Footer';
 
 export default class App extends React.Component<{}, AppState> {
   state = { 
@@ -198,13 +199,7 @@ export default class App extends React.Component<{}, AppState> {
               Adjust Time
             </div>
         </div>
-        <div id="footer">
-          Image by <a href="https://pixabay.com/users/moinzon-2433302/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1412683" className="footer-link">Michi S</a> from 
-             &nbsp;<a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1412683" className="footer-link">Pixabay</a>
-          <br />
-          <br />
-          See the project on <a href="https://github.com/hue2/Routines-Timer" className="footer-link">Github</a>
-        </div>
+       <Footer />
       </div>
     );
   }
