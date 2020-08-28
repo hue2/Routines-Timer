af0bf2a83f5e27e783d8b2fd9dea2a3cb387b0e4
hue2 <tohuem@gmail.com>
2020-08-27 21:25:57 -0500

    Added target blank to github link

--- src/components/display_components/Footer.tsx
+++ src/components/display_components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
             &nbsp;<a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1412683" className="footer-link">Pixabay</a>
             <br />
             <br />
-            See the project on <a href="https://github.com/hue2/Routines-Timer" className="footer-link">Github</a>
+            See the project on <a href="https://github.com/hue2/Routines-Timer" target="_blank" className="footer-link">Github</a>
         </div>
     );
 }
